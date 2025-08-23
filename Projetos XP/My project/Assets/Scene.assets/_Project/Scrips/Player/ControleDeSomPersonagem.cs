ae6d28567f55a79f4f2ad5fd6171666961e938e8
GabrielDyas <gabrielsilvapereiradias@gmail.com>
2025-08-23 10:17:09 -0300

    atualizando branch

---
+++ Projetos XP/My project/Assets/Scene.assets/_Project/Scrips/Player/ControleDeSomPersonagem.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+// Garante que o objeto sempre terá um AudioSource
+[RequireComponent(typeof(AudioSource))]
+public class ControleDeSomPersonagem : MonoBehaviour
+{
+    [Header("Clipes de Áudio de Passos")]
+    [Tooltip("Arraste todos os sons do pé esquerdo para esta lista.")]
+    [SerializeField] private AudioClip[] sonsDoPeEsquerdo;
+
+    [Tooltip("Arraste todos os sons do pé direito para esta lista.")]
+    [SerializeField] private AudioClip[] sonsDoPeDireito;
+
+    private AudioSource fonteDeAudio;
+
+    private void Awake()
+    {
+        fonteDeAudio = GetComponent<AudioSource>();
+        fonteDeAudio.playOnAwake = false;
+    }
+
+    public void TocarSomDePasso(string pe)
+    {
+
+        AudioClip[] listaDeSons = (pe.ToLower() == "esquerdo") ? sonsDoPeEsquerdo : sonsDoPeDireito;
+
+        if (listaDeSons.Length == 0)
+        {
+            Debug.LogWarning($"Nenhum som de passo definido para o pé '{pe}' em {gameObject.name}");
+            return;
+        }
+
+        AudioClip clipeParaTocar = listaDeSons[Random.Range(0, listaDeSons.Length)];
+
+        fonteDeAudio.PlayOneShot(clipeParaTocar);
+    }
+}
