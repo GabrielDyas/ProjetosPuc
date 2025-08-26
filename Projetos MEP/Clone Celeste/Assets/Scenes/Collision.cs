850ba8ab7da3ba06825b5f9e0214142d0689841a
Gabriel d <gabrielsilvapereiradias@gmail.com>
2025-08-26 19:31:11 -0300

    começando a melhorar a colisão

--- Projetos MEP/Clone Celeste/Assets/Scenes/Collision.cs
+++ Projetos MEP/Clone Celeste/Assets/Scenes/Collision.cs
@@ -19,10 +19,20 @@ public class Collision : MonoBehaviour
     [Space]
 
     [Header("Collision")]
-
-    public float collisionRadius = 0.25f;
-    public Vector2 bottomOffset, rightOffset, leftOffset;
+    [Space]
+    [Header("Bottom")]
+    [SerializeField] private Vector2 bottomOffset;
+    [SerializeField] private Vector2 bottomCollision;
+    [Header("Side")]
+    [Tooltip("Right and Left")]
+    [SerializeField] private Vector2  rightOffset, leftOffset;
+    [Header("Size")]
+    [SerializeField] private Vector2 sideCollision;
     private Color debugCollisionColor = Color.red;
+
+
+
+
 
     void Start()
     {
@@ -31,24 +41,27 @@ public class Collision : MonoBehaviour
 
     void Update()
     {
-        onGround = Physics2D.OverlapCircle((Vector2)transform.position + bottomOffset, collisionRadius, groundLayer);
-        onWall = Physics2D.OverlapCircle((Vector2)transform.position + rightOffset, collisionRadius, groundLayer)
-            || Physics2D.OverlapCircle((Vector2)transform.position + leftOffset, collisionRadius, groundLayer);
+        _Collison();
+    }
+    private void _Collison()
+    {
+        onGround = Physics2D.OverlapBox((Vector2)transform.position + bottomOffset, bottomCollision,0f, groundLayer);
+        onWall = Physics2D.OverlapBox((Vector2)transform.position + rightOffset, sideCollision, 0f, groundLayer)
+            || Physics2D.OverlapBox((Vector2)transform.position + leftOffset, sideCollision, 0f, groundLayer);
 
-        onRightWall = Physics2D.OverlapCircle((Vector2)transform.position + rightOffset, collisionRadius, groundLayer);
-        onLeftWall = Physics2D.OverlapCircle((Vector2)transform.position + leftOffset, collisionRadius, groundLayer);
+        onRightWall = Physics2D.OverlapBox((Vector2)transform.position + rightOffset, sideCollision, 0f, groundLayer);
+        onLeftWall = Physics2D.OverlapBox((Vector2)transform.position + leftOffset, sideCollision, 0f, groundLayer);
 
         wallSide = onRightWall ? -1 : 1;
     }
-
     void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
 
         var positions = new Vector2[] { bottomOffset, rightOffset, leftOffset };
 
-        Gizmos.DrawWireSphere((Vector2)transform.position + bottomOffset, collisionRadius);
-        Gizmos.DrawWireSphere((Vector2)transform.position + rightOffset, collisionRadius);
-        Gizmos.DrawWireSphere((Vector2)transform.position + leftOffset, collisionRadius);
+        Gizmos.DrawWireCube((Vector2)transform.position + bottomOffset, bottomCollision);
+        Gizmos.DrawWireCube((Vector2)transform.position + rightOffset, sideCollision);
+        Gizmos.DrawWireCube((Vector2)transform.position + leftOffset, sideCollision);
     }
 }
